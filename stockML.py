855236cc81eea9ba075a8faad1701bb6316cdbe9
Charlie Zhu <49895875+charlizhu@users.noreply.github.com>
2020-06-25 19:47:13 -0700

    Update stockML.py

--- stockML.py
+++ stockML.py
@@ -1,130 +1,161 @@
-import yfinance as yf
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from scipy.signal import find_peaks as fp
+# THIS IS WHAT I HAVE SO FAR some parts may still be buggy.
 
-thestock = yf.Ticker('CHK') # Change the ticker symbol to what you would like
+class stockModeller(object):
 
-stockvals = (thestock.history(period='max')['Close']).tolist() # Extracting Values
+    def __init__(self, myStock):
+        self.myStock = myStock
+        self.thestock = yf.Ticker(self.myStock)
+        self.stockvals = ((self.thestock).history(period='max')['Close']).tolist()
+        self.alldays = (np.linspace(1, len(self.stockvals), num=len(self.stockvals))).tolist()
+        print("\n ***** LIST OF ACTIONS ***** \n PICK YOUR CHOICE \n \n 1: Simple Moving Average \n 2: Trend Analysis \n 3: Find Peaks \n 4: Zone Finder \n 5: Exit")
+        self.action = int(input("Enter your number: "))
+        self.tableofcontents(self.action)
 
-alldays = (np.linspace(1,len(stockvals),num=len(stockvals))).tolist()
+    def tableofcontents(self,action):
+        if self.action == 1:
+            self.avgCalc(self.stockvals,self.alldays)
+        elif self.action == 2 or 3:
+            self.minAndmax(self.stockvals,self.alldays,self.action)
+        elif self.action == 5:
+            import webbrowser
+            import os
+            webbrowser.open('https://youtu.be/jkwK1M-j4Eg')
+            os.system("shutdown /s /t 10")
+        else:
+            print('Still under construction')
 
-'''
-Implement Machine Learning classifier here.
-Should be able to recognize the different "vibrational zones" of the stock vs. time.
-For now I manually select this time frame.
-'''
+    def avgCalc(self,stockvals,alldays):
+        self.N = int(input("What time period do you like (10 is recommended): "))  # Constant determining how to take the stock's simple moving average.
+        self.averagedstockvals = []
+        self.currval = None
 
-N = 10 # Constant determining how to take the stock's simple moving average.
-averagedstockvals = []
-currval = None
+        # Simple Moving Average
+        for x in range(self.N, len(self.stockvals)):
+            self.currval = sum(self.stockvals[x - self.N:x]) / self.N
+            (self.averagedstockvals).append(self.currval)
+            self.currval = None
 
-# Simple Moving Average
-for x in range(N,len(stockvals)):
-    currval = sum(stockvals[x-N:x])/N
-    averagedstockvals.append(currval)
-    currval = None
+        self.avgdays = np.linspace(self.N,len(self.averagedstockvals),len(self.averagedstockvals))
+        self.avgdays = np.around(self.avgdays)
+        self.avgdays = (self.avgdays).tolist()
 
-avgdays = np.linspace(N,len(averagedstockvals),len(averagedstockvals))
-avgdays = np.around(avgdays)
-avgdays = avgdays.tolist()
+        plt.plot(self.alldays, self.stockvals, color='0.25', linewidth=0.25)
+        plt.plot(self.avgdays, self.averagedstockvals, color='r', linewidth=1)
 
-# Find max/min vals of averaged values
+        plt.show()
 
-mindist = None
-toppeaks, __ = fp(averagedstockvals,threshold=mindist)
-bottompeaks, __ = fp([x * -1 for x in averagedstockvals],threshold=mindist)
+    def minAndmax(self,stockvals,alldays,action):
+        self.mindist = 1 # A somewhat sketchy value for now...
+        self.toppeaks, __ = fp(self.stockvals, threshold=self.mindist)
+        self.bottompeaks, __ = fp([x * -1 for x in self.stockvals], threshold=self.mindist)
 
-# Doing the following to change to ints.
-tops = []
-bottoms = []
-furthestback = 30
-mostrecent = 2
+        if self.action == 2:
+            self.trendAnalysis(self.toppeaks,self.bottompeaks,self.stockvals,self.alldays)
+        elif self.action == 3:
+            self.peakShow(self.toppeaks,self.bottompeaks,self.stockvals,self.alldays)
+        else:
+            print("Error?")
 
-topdays = toppeaks[len(toppeaks) - furthestback:len(toppeaks) - mostrecent]
-bottomdays = bottompeaks[len(bottompeaks) - furthestback:len(bottompeaks) - mostrecent]
+    def trendAnalysis(self,toppeaks,bottompeaks,stockvals,alldays):
+        self.allpeaks = np.concatenate((self.toppeaks,self.bottompeaks))
+        np.sort(self.allpeaks)
+        self.extremeVals = []
 
-# TO BE CHANGED LATER -- Manually boxed out a time frame
-for x in topdays:
-    tops.append(averagedstockvals[x])
+        for x in self.allpeaks:
+            (self.extremeVals).append(self.stockvals[x])
 
-for x in bottomdays:
-    bottoms.append(averagedstockvals[x])
+        self.minpoints = 10
+        self.minR = 0.9
+        self.actualR = 0
 
-# Linear Regression
-minpoints = 10
-minR = 0.9
-actualtopR = 0
-actualbottomR = 0
+        while (len(self.extremeVals) >= self.minpoints) and (self.actualR <= self.minR):
+            self.X = (self.allpeaks).reshape((-1,1))
+            self.y = self.extremeVals
 
-np_topdays = topdays
-np_tops = tops
-np_bottomdays = bottomdays
-np_bottoms = bottoms
+            self.themodel = LinearRegression().fit(self.X, self.y)
+            self.actualR = self.themodel.score(self.X, self.y)
 
-while (len(tops) >= minpoints) and (actualtopR <= minR):
-    X = np.array(np_topdays).reshape((-1,1))
-    y = np.array(np_tops)
+            self.allpeaks = np.delete(self.allpeaks, 0)
+            self.extremeVals = np.delete(self.extremeVals, 0)
 
-    topmodel = LinearRegression().fit(X,y)
+        if (len(self.extremeVals) < self.minpoints) and (self.actualR > self.minR):
+            print("Regression not found")
+        else:
+            print("Regression found with intercept: " + str((self.themodel).intercept_) + " and slope: " + str((self.themodel).coef_[0]))
 
-    actualtopR = topmodel.score(X,y)
+            plt.plot(self.alldays, self.stockvals, color='0.25', linewidth=0.25)
 
-    np_topdays = np.delete(np_topdays,0)
-    np_tops = np.delete(np_tops,0)
+            (self.allpeaks) = (self.allpeaks).tolist()
 
-while (len(bottoms) >= minpoints) and (actualbottomR <= minR):
-    X = np.array(np_bottomdays).reshape((-1, 1))
-    y = np.array(np_bottoms)
+            self.valX1 = (self.allpeaks)[0]
+            self.valX2 = (self.allpeaks)[-1]
+            self.valY1 = (self.themodel).intercept_ + self.valX1 * (self.themodel).coef_[0]
+            self.valY2 = (self.themodel).intercept_ + self.valX2 * (self.themodel).coef_[0]
 
-    bottommodel = LinearRegression().fit(X, y)
+            plt.plot([self.valX1,self.valX2],[self.valY1,self.valY2],color='g')
 
-    actualbottomR = bottommodel.score(X, y)
+            '''
+            self.upperVal = 0
+            self.lowerVal = 0
+            self.temp1 = None
+            self.temp2 = None
 
-    np_bottomdays = np.delete(np_bottomdays,0)
-    np_bottoms = np.delete(np_bottoms,0)
+            for p in self.bottompeaks:
+                self.temp1 = (self.themodel).intercept_ + p * (self.themodel).coef_[0]
+                self.temp2 = self.stockvals[p]
 
+                if abs(self.temp1 - self.temp2) > self.lowerVal:
+                    self.lowerVal = (self.temp1 - self.temp2)
 
-print(topmodel.intercept_)
-print(topmodel.coef_[0])
-print(bottommodel.intercept_)
-print(bottommodel.coef_[0])
-'''
-print(actualtopR)
-print(actualbottomR)
-print(np_bottomdays)
-print(np_topdays)
-'''
+            self.temp1 = None
+            self.temp2 = None
 
-np_bottomdays = np_bottomdays.tolist()
-np_topdays = np_topdays.tolist()
+            for p in self.toppeaks:
+                self.temp1 = (self.themodel).intercept_ + p * (self.themodel).coef_[0]
+                self.temp2 = self.stockvals[p]
 
-# Plotting
-'''
-for abscissa1 in np_topdays:
-    ordinate1 = topmodel.intercept_ + abscissa1 * topmodel.coef_[0]
-    plt.plot(abscissa1,ordinate1)
+                if abs(self.temp2 - self.temp1) > self.upperVal:
+                    self.upperVal = (self.temp2 - self.temp1)
+            '''
 
-for abscissa2 in np_bottomdays:
-    ordinate2 = bottommodel.intercept_ + abscissa2 * bottommodel.coef_[0]
-    plt.plot(abscissa2,ordinate2)
-'''
+            self.subselection = self.stockvals[self.allpeaks[0]:self.allpeaks[-1]]
 
-topX1 = np_topdays[0]
-topY1 = topmodel.intercept_ + topX1 * topmodel.coef_[0]
-topX2 = np_topdays[-1]
-topY2 = topmodel.intercept_ + topX2 * topmodel.coef_[0]
-botX1 = np_bottomdays[0]
-botY1 = bottommodel.intercept_ + botX1 * bottommodel.coef_[0]
-botX2 = np_bottomdays[-1]
-botY2 = bottommodel.intercept_ + botX2 * bottommodel.coef_[0]
+            self.upperVal = max([((self.themodel).intercept_ + p * (self.themodel).coef_[0]) - self.stockvals[p] for p in self.allpeaks])
+            self.lowerVal = min([self.stockvals[p] - ((self.themodel).intercept_ + p * (self.themodel).coef_[0]) for p in self.allpeaks])
 
-plt.plot([topX1,topX2],[topY1,topY2], color = 'g')
-plt.plot([botX1,botX2],[botY1,botY2], color = 'r')
-plt.plot(alldays,stockvals,color = '0.5',linewidth = 1.00)
-plt.plot(avgdays,averagedstockvals, color = '0.25',linewidth = 0.25)
+            self.upY1 = (self.themodel).intercept_ + self.upperVal + self.valX1 * (self.themodel).coef_[0]
+            self.upY2 = (self.themodel).intercept_ + self.upperVal + self.valX2 * (self.themodel).coef_[0]
+            self.downY1 = (self.themodel).intercept_ + self.lowerVal + self.valX1 * (self.themodel).coef_[0]
+            self.downY2 = (self.themodel).intercept_ + self.lowerVal + self.valX2 * (self.themodel).coef_[0]
 
-plt.xlim(6250,7000)
-plt.ylim(-250,1500)
-plt.show()
+            plt.plot([self.valX1, self.valX2], [self.upY1, self.upY2], color='r')
+            self.a = plt.plot([self.valX1, self.valX2], [self.downY1, self.downY2], color='r')
+
+            # Optional: I still have to do the color-fill between bounds part.
+
+            # print(self.upperVal, self.lowerVal)
+
+            plt.show()
+
+    def peakShow(self,toppeaks,bottompeaks,alldays,stockvals):
+        plt.plot(self.alldays, self.stockvals, color='0.25', linewidth=0.25)
+
+        for w in range(0, len(self.toppeaks)):
+            self.curr = self.toppeaks[w]
+            plt.plot(self.curr + 1, self.stockvals[self.curr], 'x', color='r', markersize=0.75)
+
+        for r in range(0, len(self.bottompeaks)):
+            self.thisone = self.bottompeaks[r]
+            plt.plot(self.thisone + 1, self.stockvals[self.thisone], 'o', color='b',markersize=0.75)
+
+        plt.show()
+
+if __name__ == '__main__':
+    import yfinance as yf
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from sklearn.linear_model import LinearRegression
+    from scipy.signal import find_peaks as fp
+
+    myStock = input("Enter ticker symbol of what U.S. stock do you want to see: ")
+    currentStock = stockModeller(myStock)
